e6dc3eaeb2973904ebf8efdee34955b7fdad03e0
MandoD1 <damodmo1110@gmail.com>
2026-04-04 23:02:27 -0500

    eliminé una clase que tenia de prueba que se me olvidó quitar

--- src/main/java/org/example/ax0006/s.java
+++
@@ -1,9 +0,0 @@
-package org.example.ax0006;
-
-import org.h2.tools.Server;
-
-public class s {
-    public static void main(String[] args) throws Exception {
-        Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
-    }
-}
